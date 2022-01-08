60186cd6b3191d6b0160972474b0da1c34d670f8
chentao.arthur <chentao.arthur@bytedance.com>
2022-01-08 17:52:53 +0800

    test: add test case

--- test/script/case/exportNamespace/m2.js
+++ test/script/case/exportNamespace/m2.js
@@ -6,7 +6,11 @@ export { name } from './m3.js';
 export { name as n1 } from './m3.js';
 export { name as nn, name as n2 } from './m3.js';
 
+import { name } from './m3.js';
+
 export const _name = 'm2';
 
 const d = 1;
 export { d };
+
+expect(name).toBe('m3');
